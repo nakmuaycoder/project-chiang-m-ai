c2db2952500a2cbf6f01cad5aa0e17847ffc4811
นักมวยโคเดอร์ <feherolivier@gmail.com>
2026-01-18 17:16:57 +0100

    Add secrets

---
+++ src/config.py
@@ -0,0 +1,15 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    INTERVALS_ATHLETE_ID: str
+    INTERVALS_API_KEY: SecretStr
+    GOOGLE_API_KEY: SecretStr
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+
+settings = Settings()
